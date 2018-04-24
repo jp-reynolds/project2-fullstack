c8e0641358718f979003a6b242e2cb6cc1333b40
jp-reynolds <jpreynolds215@gmail.com>
2018-04-24 13:03:37 -0600

    deploying heroku

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const bcrypt       = require('bcrypt');
 const app          = express();
 var   User         = require('./models/users');
 
-mongoose.connect("mongodb://localhost/project2");
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect("mongodb://localhost/project2");
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
